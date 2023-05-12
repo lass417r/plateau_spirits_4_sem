144d3c8dc2298994243605024ae561e05910f148
Lasse Godtkjaer <lass417r@stud.kea.dk>
2023-05-12 14:20:10 +0200

    font

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono">
         <h1>PLATEAU SPIRITS</h1>
-        <Image src={ginImg} width={1365} height={2048} alt="Picture of Gin bottle"></Image>
       </div>
     </main>
   );
