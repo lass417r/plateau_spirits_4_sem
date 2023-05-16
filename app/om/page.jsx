4a0ec46aaa8374716217850df70781129221b58a
Lasse Godtkjaer <lass417r@stud.kea.dk>
2023-05-16 12:52:56 +0200

    remove motion

--- app/om/page.jsx
+++ app/om/page.jsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Historie from "./Historie";
 import Slideshow from "./Slideshow";
-import { animate, stagger, inView, scroll, timeline } from "motion";
 
 export default function Shop() {
   return (
