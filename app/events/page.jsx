ba9c73b0a957fd8558e754b625356aaf9b4a7edb
Lasse Godtkjaer <lass417r@stud.kea.dk>
2023-05-23 11:46:24 +0200

    responsivt

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -14,142 +14,73 @@ import grid from "../../public/assets/grid.svg";
 import events from "../../public/assets/events.svg";
 import deksel from "../../public/assets/deksel_skrift.svg";
 
-export default function Shop() {
+export default function Events() {
   return (
     <>
-      <section className="mx-20 flex min-h-screen flex-row items-start justify-between md:mb-20">
-        <div className="sticky top-3 basis-1/2">
+      <section className="flex min-h-screen flex-col items-start justify-between md:mx-20 md:mb-20 md:flex-row">
+        <div className="top-3 md:sticky md:basis-1/2">
           <div className="flex flex-col items-start gap-5 p-5">
-            <h1 className="font-tyk text-wh1 text-orangebase">
-              Plateau Events{" "}
-            </h1>
+            <h1 className="font-tyk text-wh1 text-orangebase">Plateau Events</h1>
             <h2 className="font-tyk text-wh2">Events & Smagninger</h2>
             <p className="font-copy text-copy lg:max-w-[80%]">
-              Vi tilbyder muligheden for at afholde events og arrangementer i
-              vores showroom i Kødbyen.
+              Vi tilbyder muligheden for at afholde events og arrangementer i vores showroom i Kødbyen.
               <br /> <br />
-              Vores lokaler er velegnede til både private og erhvervsmæssige
-              arrangementer, hvor I kan nyde vores unikke spiritus produkter
-              under arrangementet. Vi tilpasser gerne arrangementet til jeres
-              ønsker og behov og tilbyder også muligheden for at inkludere en
-              rundvisning i vores destilleri og en præsentation af
-              destillationsprocessen.
+              Vores lokaler er velegnede til både private og erhvervsmæssige arrangementer, hvor I kan nyde vores unikke spiritus produkter under arrangementet. Vi tilpasser gerne arrangementet til
+              jeres ønsker og behov og tilbyder også muligheden for at inkludere en rundvisning i vores destilleri og en præsentation af destillationsprocessen.
             </p>
             <Button href="/shop" text="Book Event"></Button>
           </div>
         </div>
-        <div className="relative flex basis-1/2 flex-col items-center gap-5 p-5">
-          <Image
-            className=" absolute  z-0 "
-            src={grid}
-            width={835}
-            height={1106}
-            alt="Illustrativt element"
-          />
-          <motion.div
-            className="relative inline-block md:mt-32"
-            initial={{ opacity: 0.2 }}
-            whileInView={{ opacity: 1, transition: { duration: 1 } }}
-          >
-            <Image
-              className=" absolute -right-16 -top-28 z-10"
-              src={events}
-              width={318}
-              height={456}
-              alt="Illustrativt element"
-            />
-            <Image
-              className=" z-0 md:mt-5"
-              src={pic8}
-              width={500}
-              height={500}
-              alt="Billede af lokation"
-            />
+        <div className="relative flex flex-col items-center gap-5 p-5 md:basis-1/2">
+          <Image className=" absolute  z-0 " src={grid} width={835} height={1106} alt="Illustrativt element" />
+          <motion.div className="relative inline-block md:mt-32" initial={{ opacity: 0.2 }} whileInView={{ opacity: 1, transition: { duration: 1 } }}>
+            <Image className=" absolute -right-16 -top-28 z-10" src={events} width={318} height={456} alt="Illustrativt element" />
+            <Image className=" z-0 md:mt-5" src={pic8} width={500} height={500} alt="Billede af lokation" />
           </motion.div>
-          <motion.div
-            className="z-10 md:-mt-20 md:ml-20"
-            initial={{ opacity: 0.2 }}
-            whileInView={{ opacity: 1, transition: { duration: 1 } }}
-          >
-            <Image
-              className="z-10"
-              src={pic11}
-              width={500}
-              height={500}
-              alt="Billede af lokation"
-            />
+          <motion.div className="z-10 md:-mt-20 md:ml-20" initial={{ opacity: 0.2 }} whileInView={{ opacity: 1, transition: { duration: 1 } }}>
+            <Image className="z-10" src={pic11} width={500} height={500} alt="Billede af lokation" />
           </motion.div>
-          <motion.div
-            className="z-10 md:-mt-20 md:mr-20"
-            initial={{ opacity: 0.2 }}
-            whileInView={{ opacity: 1, transition: { duration: 1 } }}
-          >
-            <Image
-              className="fotoinview z-0"
-              src={pic10}
-              width={500}
-              height={500}
-              alt="Billede af lokation"
-            />
+          <motion.div className="z-10 md:-mt-20 md:mr-20" initial={{ opacity: 0.2 }} whileInView={{ opacity: 1, transition: { duration: 1 } }}>
+            <Image className="fotoinview z-0" src={pic10} width={500} height={500} alt="Billede af lokation" />
           </motion.div>
         </div>
       </section>
-      <section className="mx-20 mt-32 flex flex-row items-start gap-20 md:mb-20">
-        <motion.div
-          className="flex basis-1/2 justify-end"
-          initial={{ opacity: 0.2 }}
-          whileInView={{ opacity: 1, transition: { duration: 1 } }}
-        >
-          <Image
-            className=""
-            src={pic9}
-            width={500}
-            height={500}
-            alt="Billede af lokation"
-          />
+      <section className="mb-10   flex flex-col items-start gap-20 p-5 md:mx-20 md:mb-20 md:flex-row">
+        <motion.div className="flex basis-1/2 justify-end" initial={{ opacity: 0.2 }} whileInView={{ opacity: 1, transition: { duration: 1 } }}>
+          <Image className="" src={pic9} width={500} height={500} alt="Billede af lokation" />
         </motion.div>
         <div className="basis-1/2 lg:max-w-[80%]">
           <h3 className="font-tyk text-wh3">Typer af events</h3>
           <p className="lg:max-w-[80%]">
             <br />
-            Vi tilbyder forskellige typer af events, og er altid fleksible for
-            at imødekomme jeres ønsker.
+            Vi tilbyder forskellige typer af events, og er altid fleksible for at imødekomme jeres ønsker.
             <br />
             <br />
-            Her ses et bud på hvordan et event kan struktureres i vores
-            showroom.
+            Her ses et bud på hvordan et event kan struktureres i vores showroom.
             <br />
             <br />
-            Arrangementet starter med velkomstdrinks og en introduktion til
-            programmet. Derefter er der en rundvisning i vores showroom og en
-            gennemgang af destillationsprocessen.
+            Arrangementet starter med velkomstdrinks og en introduktion til programmet. Derefter er der en rundvisning i vores showroom og en gennemgang af destillationsprocessen.
             <br />
             <br />
-            Efterfølgende afholder vi en cocktailworkshop eller en gin-smagning,
-            hvor vores produkter kan smages og udforskes af jer.
+            Efterfølgende afholder vi en cocktailworkshop eller en gin-smagning, hvor vores produkter kan smages og udforskes af jer.
             <br />
             <br />
-            Vi runder aftenen af med mad serveret fra en af restauranterne i
-            Kødbyen.
+            Vi runder aftenen af med mad serveret fra en af restauranterne i Kødbyen.
           </p>
         </div>
       </section>
-      <section className=" flex min-h-screen flex-col items-center justify-between  bg-matte200">
+      <section className=" flex min-h-screen flex-col items-center justify-between  bg-matte200 pt-10">
         <div className="z w-full">
           <TextBanner text="BOOK DIT NÆSTE EVENT! &nbsp; - &nbsp; BOOK DIT NÆSTE EVENT!  &nbsp; - &nbsp; BOOK DIT NÆSTE EVENT!"></TextBanner>
         </div>
-        <div className=" flex">
-          <div className="kontaktformular  mx-20 flex basis-1/2 flex-col gap-5 md:my-20">
+        <div className=" mb-10 flex">
+          <div className="kontaktformular  flex flex-col gap-5 md:mx-20 md:my-20 md:basis-2/3">
             <h3 className="font-tyk text-wh3 text-yellowbase ">Book her</h3>
             <KontaktEvents className="z-10"></KontaktEvents>
           </div>
-          <Image
-            className="basis-1/2"
-            src={events}
-            alt="Illustrativt element"
-            width={400}
-            height={400}
-          ></Image>
+          <div className="overflow-hidden">
+            <Image className="hidden basis-1/3 md:block" src={events} alt="Illustrativt element" width={400} height={400}></Image>
+          </div>
         </div>
       </section>
     </>
--- app/virksomhed/page.jsx
+++ app/virksomhed/page.jsx
@@ -7,24 +7,14 @@ import pic9 from "../../public/images/pic9.png";
 export default function Shop() {
   return (
     <>
-      <section className="mx-20 flex md:mt-20">
+      <section className="flex flex-col p-5 md:mx-20 md:mt-20 md:flex-row">
         <div className="basis-1/2 ">
-          <Image
-            className="fotoinview z-0"
-            src={filler}
-            width={500}
-            height={500}
-            alt="Billede af ejerne"
-          />
+          <Image className="fotoinview z-0" src={filler} width={500} height={500} alt="Billede af ejerne" />
         </div>
-        <div className="flex basis-1/2 flex-col items-start gap-5">
-          <h1 className="font-tyk text-wh1 text-blackbase lg:pt-20 ">
-            Designet til din virksomhed!
-          </h1>
+        <div className="flex basis-1/2 flex-col items-start gap-5 ">
+          <h1 className="font-tyk text-wh1 text-blackbase lg:pt-20 ">Designet til din virksomhed!</h1>
           <p className="font-copy text-copy text-blackbase lg:max-w-[80%]">
-            Vi tilbyder private label, til din virksomhed. Få dit navn på
-            flasken og server premium alkohol for dine gæste r. Vi har lavet
-            private label for flere restauranter, hoteller samt andre
+            Vi tilbyder private label, til din virksomhed. Få dit navn på flasken og server premium alkohol for dine gæste r. Vi har lavet private label for flere restauranter, hoteller samt andre
             virksomhedstyper.
           </p>
           <div className="pt-6">
@@ -32,27 +22,20 @@ export default function Shop() {
           </div>
         </div>
       </section>
-      <section className="mx-20 flex items-center justify-center gap-20 md:mt-28">
+      <section className="my-10 flex flex-col items-center justify-center gap-10 p-5 md:mx-20 md:my-28 md:flex-row md:gap-20">
         <div className="flex basis-1/2 flex-col items-start gap-5">
-          <h2 className="font-tyk text-wh2 text-blackbase lg:max-w-[80%]">
-            Hør om Gorilla’s oplevelse
-          </h2>
+          <h2 className="font-tyk text-wh2 text-blackbase lg:max-w-[80%]">Hør om Gorilla’s oplevelse</h2>
           <p className="font-copy text-copy text-blackbase lg:max-w-[80%]">
-            Vi havde fornøjelsen af at arbejde sammen med Plateau Spirits, da vi
-            ønskede at få en unik chartreuse udviklet til vores restaurant. Vi
-            tog kontakt til Tonny Svensson og forklarede vores ønsker og idéer
-            til vores chartreuse. Tonny gjorde hvad han gør bedst og skabte den
-            perfekte chartreuse til vores restaurant Gorilla. Det er nu blevet
-            fast inventar i vores restaurant og vores gæster elsker, når vi
-            serverer den for dem.
+            Vi havde fornøjelsen af at arbejde sammen med Plateau Spirits, da vi ønskede at få en unik chartreuse udviklet til vores restaurant. Vi tog kontakt til Tonny Svensson og forklarede vores
+            ønsker og idéer til vores chartreuse. Tonny gjorde hvad han gør bedst og skabte den perfekte chartreuse til vores restaurant Gorilla. Det er nu blevet fast inventar i vores restaurant og
+            vores gæster elsker, når vi serverer den for dem.
             <br />
           </p>
-          <p className="font-copy text-copy font-bold text-blackbase lg:max-w-[80%]">
-            - Restaurantchef Gorilla, Hans Hansen
-          </p>
+          <p className="font-copy text-copy font-bold text-blackbase lg:max-w-[80%]">- Restaurantchef Gorilla, Hans Hansen</p>
         </div>
-        <div className="basis-1/2">
+        <div className="h-full w-full">
           <iframe
+            className="aspect-video h-full hover:aspect-square"
             width="560"
             height="315"
             src="https://www.youtube.com/embed/HEUMrGoBeUs"
@@ -63,25 +46,15 @@ export default function Shop() {
           ></iframe>
         </div>
       </section>
-      <section className="mx-20 flex items-center justify-center gap-20 md:my-28">
-        <div className="basis-1/2">
-          <Image
-            className=""
-            src={pic9}
-            width={500}
-            height={500}
-            alt="Billede af lokation"
-          />
+      <section className="my-10 flex flex-col items-center justify-center gap-10 p-5 md:mx-20 md:my-28 md:flex-row md:gap-20">
+        <div className="md:basis-1/2">
+          <Image className="" src={pic9} width={500} height={500} alt="Billede af lokation" />
         </div>
-        <div className="flex basis-1/2 flex-col items-start gap-5">
-          <h2 className="font-tyk text-wh2 text-blackbase">
-            Events til din virksomhed
-          </h2>
+        <div className="flex flex-col items-start gap-5 md:basis-1/2">
+          <h2 className="font-tyk text-wh2 text-blackbase">Events til din virksomhed</h2>
           <p className="font-copy text-copy text-blackbase lg:max-w-[80%]">
-            Vi afholder jævnligt gin-smagninger, foredrag og stiller anderledes
-            mødelokaler til rådighed. Vi stræber efter at give jer en unik og
-            mindeværdig oplevelse, der vil styrke jeres team og samarbejde.
-            Kontakt os for at lære mere om vores skræddersyede events.
+            Vi afholder jævnligt gin-smagninger, foredrag og stiller anderledes mødelokaler til rådighed. Vi stræber efter at give jer en unik og mindeværdig oplevelse, der vil styrke jeres team og
+            samarbejde. Kontakt os for at lære mere om vores skræddersyede events.
           </p>
           <div className="pt-6">
             <Button href="/events" text="Events"></Button>
