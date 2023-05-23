d186b08b2c2e1057b94ae169ca98d34a18f9c074
Lasse Godtkjaer <lass417r@stud.kea.dk>
2023-05-23 18:55:41 +0200

    Productpic hover fix

--- components/productShop.jsx
+++ components/productShop.jsx
@@ -11,18 +11,14 @@ const ProductShop = ({ product }) => {
   return product.price === 500 ? (
     <div className=" grid auto-rows-auto gap-2 p-5 md:col-span-2" key={product.id}>
       <Link className="" key={product.id} href={`/shop/${product.slug}`}>
-        <motion.div
-          className="flex aspect-[2/1] h-auto w-full"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-          whileHover={{
-            scale: 1.02,
-            opacity: 0.8,
-            transition: { duration: 1, ease: [0.43, 0.13, 0.23, 0.96] },
-          }}
-        >
-          <Image className="items-center justify-end" src={isHovered ? product.images[1].file.url : product.images[0].file.url} alt={product.name} width={800} height={400} />
-        </motion.div>
+        <div className="relative flex aspect-[2/1] h-auto w-full">
+          <motion.div className="absolute" initial={{ opacity: 1 }} whileHover={{ opacity: 0 }} transition={{ duration: 1, ease: [0.43, 0.13, 0.23, 0.96] }}>
+            <Image src={product.images[0].file.url} alt={product.name} width={800} height={400} />
+          </motion.div>
+          <motion.div className="absolute" initial={{ opacity: 0 }} whileHover={{ opacity: 1 }} transition={{ duration: 1, ease: [0.43, 0.13, 0.23, 0.96] }}>
+            <Image src={product.images[1].file.url} alt={product.name} width={800} height={400} />
+          </motion.div>
+        </div>
       </Link>
       <div className="text-blackbase ">
         <h2 className="font-tyk text-copy">{product.name}</h2>
@@ -35,19 +31,14 @@ const ProductShop = ({ product }) => {
   ) : (
     <div className="op col-span-1 grid auto-rows-auto gap-2 p-5" key={product.id}>
       <Link className="col-span-1" key={product.id} href={`/shop/${product.slug}`}>
-        <motion.div
-          className="flex aspect-[1/1] h-auto w-full "
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-          initial={isHovered ? "opacity: 100" : "opacity: 100"}
-          whileHover={{
-            scale: 1.01,
-            opacity: 0.7,
-            transition: { duration: 1, ease: [0.43, 0.13, 0.23, 0.96] },
-          }}
-        >
-          <Image className="items-center justify-end" src={isHovered ? product.images[1].file.url : product.images[0].file.url} alt={product.name} width={400} height={400} />
-        </motion.div>
+        <div className="relative flex aspect-[1/1] h-auto w-full">
+          <motion.div className="absolute" initial={{ opacity: 1 }} whileHover={{ opacity: 0 }} transition={{ duration: 1, ease: [0.43, 0.13, 0.23, 0.96] }}>
+            <Image src={product.images[0].file.url} alt={product.name} width={800} height={400} />
+          </motion.div>
+          <motion.div className="absolute" initial={{ opacity: 0 }} whileHover={{ opacity: 1 }} transition={{ duration: 1, ease: [0.43, 0.13, 0.23, 0.96] }}>
+            <Image src={product.images[1].file.url} alt={product.name} width={800} height={400} />
+          </motion.div>
+        </div>
       </Link>
       <div className="text-blackbase">
         <h2 className="font-tyk text-copy">{product.name}</h2>
