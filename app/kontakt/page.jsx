3a4d14b9db027adf25af9817455e0e6be3a131d2
Christian Ohm <chri29kx@stud.kea.dk>
2023-05-24 13:01:24 +0200

    Ændringer

--- app/kontakt/page.jsx
+++ app/kontakt/page.jsx
@@ -25,33 +25,59 @@ export default function Shop() {
           </p>
         </div>
       </section>
-      <section className=" -mt-32 bg-clay">
-        <div className="mx-20 flex gap-10 pt-64">
-          <div className="relative flex basis-1/2 flex-col gap-10">
-            <h1 className="font-tyk text-wh1 text-blackbase">Kontaktinfo</h1>
-            <div>
-              <h5 className="text-wh font-tyk text-blackbase"> Email: </h5>
-              <Link className="font-copy text-copy  text-blackbase" target="_blank" href="mailto:info@plateauspirits.com">
-                Info@plateauspirits.com
-              </Link>
-            </div>
-            <div>
-              <h5 className="text-wh font-tyk text-blackbase"> Telefon: </h5>
-              <Link className="font-copy text-copy  text-blackbase" href="tel:+4522422020">
-                +45 22 42 20 20
-              </Link>
-            </div>
-            <div className="absolute right-1/4 top-1/3">
-              <Image src={deksel} width={442} height={439} alt="Dekorativ illustration"></Image>
-            </div>
+      <section className=" px-24 py-24 bg-clay ">
+    <h2 className="font-tyk text-center  text-wh2">Kontakt information</h2>
+    <div className="lg:flex  flex-col lg:flex-row justify-between ">
+        <div className="flex justify-center flex-col">
+          <div className="flex justify-center p-10">
           </div>
+           <h3 className="  font-tyk text-wh4 flex justify-center lg:mb-8 lg:justify-center">
+         Mail
+           </h3>
+          <p className="font-copy flex justify-center lg:justify-center text-center text-copy lg:w-80">Info@plateauspirits.com
+          </p>
+        </div>
 
-          <div className="basis-1/2">
-            <h1 className="font-tyk text-wh1 text-blackbase">Kontaktformular</h1>
-            <Kontakt></Kontakt>
+        <div className="flex justify-center flex-col">
+        <div className="flex justify-center p-10">
+         
+          </div>
+           <h3 className="font-tyk text-wh4 flex text-center justify-center lg:mb-8 lg:justify-center">
+          Telefon
+           </h3>
+          <p className="font-copy flex justify-center lg:justify-center text-center text-copy lg:w-80">+45 22 42 20 20
+          </p>
+        </div>
+        <div className="flex justify-center flex-col">
+        <div className="flex justify-center p-10">
+          
+          </div>
+           <h3 className="  font-tyk text-wh4 flex justify-center lg:mb-8 lg:justify-center">
+          Adresse
+           </h3>
+          <p className="font-copy flex justify-center lg:justify-center text-center text-copy lg:w-80">Flæsketorvet 6, 1711 København
+          </p>
+        </div>
+        </div>
+      </section>
+      <section className="">
+        <div className="flex p-7 justify-center">
+          <div className="flex-col justify-center">
+          <h2 className="font-tyk text-wh1 text-blackbase">Kontaktformular</h2>
+          <Kontakt></Kontakt>   
+          {/* <Image src={deksel} width={442} height={439} alt="Dekorativ illustration"></Image> */}
           </div>
         </div>
       </section>
+      {/* <section className=" -mt-32 ">
+        <div className=" flex flex-col justify-center gap-10 pt-64">
+            <h2 className="font-tyk text-wh1 text-blackbase">Kontaktformular</h2>
+            <Kontakt></Kontakt>    
+          <div className=" ">
+              <Image src={deksel} width={442} height={439} alt="Dekorativ illustration"></Image>
+            </div>
+        </div>
+      </section> */}
       <section className=" bg-clay py-10 ">
         <TextBanner text="BESØG OS PÅ FLÆSKETORVET 6, KØDBYEN &nbsp; - &nbsp; BESØG OS PÅ FLÆSKETORVET 6, KØDBYEN"></TextBanner>
       </section>
