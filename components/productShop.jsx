e47600850a2a8bc65121f567149501328a39429a
Lasse Godtkjaer <lass417r@stud.kea.dk>
2023-05-24 11:59:51 +0200

    shop center

--- components/productShop.jsx
+++ components/productShop.jsx
@@ -9,40 +9,50 @@ const ProductShop = ({ product }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return product.price === 500 ? (
-    <div className=" grid auto-rows-auto gap-2 p-5 md:col-span-2" key={product.id}>
+    <div className=" grid  gap-2 p-5 md:col-span-2" key={product.id}>
       <Link className="" key={product.id} href={`/shop/${product.slug}`}>
-        <div className="relative flex aspect-[2/1] h-auto w-full">
-          <motion.div className="absolute" initial={{ opacity: 1 }} whileHover={{ opacity: 0 }} transition={{ duration: 1, ease: [0.43, 0.13, 0.23, 0.96] }}>
-            <Image src={product.images[0].file.url} alt={product.name} width={800} height={400} />
+        <div className="relative flex aspect-[1/1]  h-full w-full">
+          <motion.div
+            className="absolute  h-full w-full object-cover object-center"
+            initial={{ opacity: 1 }}
+            whileHover={{ opacity: 0, scale: 1.005 }}
+            transition={{ duration: 1, ease: [0.43, 0.13, 0.23, 0.96] }}
+          >
+            <Image src={product.images[0].file.url} alt={product.name} width={8192} height={4096} />
           </motion.div>
-          <motion.div className="absolute" initial={{ opacity: 0 }} whileHover={{ opacity: 1 }} transition={{ duration: 1, ease: [0.43, 0.13, 0.23, 0.96] }}>
-            <Image src={product.images[1].file.url} alt={product.name} width={800} height={400} />
+          <motion.div className="absolute object-cover object-center" initial={{ opacity: 0 }} whileHover={{ opacity: 1, scale: 1.005 }} transition={{ duration: 1, ease: [0.43, 0.13, 0.23, 0.96] }}>
+            <Image src={product.images[1].file.url} alt={product.name} width={8192} height={4096} />
           </motion.div>
         </div>
       </Link>
-      <div className="text-blackbase ">
-        <h2 className="font-tyk text-copy">{product.name}</h2>
-        <p className="font-copy text-copy">{product.price}</p>
+      <div className="flex flex-col items-center gap-1 text-blackbase">
+        <h5 className="font-tyk text-wh5">{product.name}</h5>
+        <p className="font-copy text-copy">{product.price} DKK</p>
         <div className="mt-5">
           <Button href={`/shop/${product.slug}`} text={"SE PRODUKT"}></Button>
         </div>
       </div>
     </div>
   ) : (
-    <div className="op col-span-1 grid auto-rows-auto gap-2 p-5" key={product.id}>
+    <div className=" col-span-1 grid  gap-2 p-5" key={product.id}>
       <Link className="col-span-1" key={product.id} href={`/shop/${product.slug}`}>
-        <div className="relative flex aspect-[1/1] h-auto w-full">
-          <motion.div className="absolute" initial={{ opacity: 1 }} whileHover={{ opacity: 0 }} transition={{ duration: 1, ease: [0.43, 0.13, 0.23, 0.96] }}>
-            <Image src={product.images[0].file.url} alt={product.name} width={800} height={400} />
+        <div className="relative flex aspect-[1/1]  h-full w-full">
+          <motion.div
+            className="absolute  h-full w-full object-cover object-center"
+            initial={{ opacity: 1 }}
+            whileHover={{ opacity: 0, scale: 1.005 }}
+            transition={{ duration: 1, ease: [0.43, 0.13, 0.23, 0.96] }}
+          >
+            <Image src={product.images[0].file.url} alt={product.name} width={4096} height={4096} />
           </motion.div>
-          <motion.div className="absolute" initial={{ opacity: 0 }} whileHover={{ opacity: 1 }} transition={{ duration: 1, ease: [0.43, 0.13, 0.23, 0.96] }}>
-            <Image src={product.images[1].file.url} alt={product.name} width={800} height={400} />
+          <motion.div className="absolute object-cover object-center" initial={{ opacity: 0 }} whileHover={{ opacity: 1, scale: 1.005 }} transition={{ duration: 1, ease: [0.43, 0.13, 0.23, 0.96] }}>
+            <Image src={product.images[1].file.url} alt={product.name} width={4096} height={4096} />
           </motion.div>
         </div>
       </Link>
-      <div className="text-blackbase">
-        <h2 className="font-tyk text-copy">{product.name}</h2>
-        <p className="font-copy text-copy">{product.price}</p>
+      <div className="flex flex-col items-center gap-1 text-blackbase ">
+        <h5 className="font-tyk text-wh5">{product.name}</h5>
+        <p className="font-copy text-copy">{product.price} DKK</p>
         <div className="mt-5">
           <Button href={`/shop/${product.slug}`} text={"SE PRODUKT"}></Button>
         </div>
