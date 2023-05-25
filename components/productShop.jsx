0cfe94d7c3e9698dba83ebbe6845064cdc183a15
Lasse Godtkjaer <lass417r@stud.kea.dk>
2023-05-25 14:37:30 +0200

    cookiebox

--- components/productShop.jsx
+++ components/productShop.jsx
@@ -6,8 +6,15 @@ import Link from "next/link";
 import Button from "./Button";
 
 const ProductShop = ({ product }) => {
+  {
+    /* Her opretter vi et state der bliver opdateret hvis brugeren hover over produktbilledet */
+  }
   const [isHovered, setIsHovered] = useState(false);
-
+  {
+    /* Herinde tjekker vi om prisen er præcis 500 (fordi så er det en pakke)
+     Hvis det er en pakke, fylder produktet 2 coloner i gridet og 
+     hvis det er en flaske  fylder den kun 1.*/
+  }
   return product.price === 500 ? (
     <div className=" grid  gap-2 p-5 md:col-span-2" key={product.id}>
       <Link key={product.id} href={`/shop/${product.slug}`}>
@@ -44,6 +51,7 @@ const ProductShop = ({ product }) => {
     <div className=" grid  gap-2 p-5 md:col-span-1" key={product.id}>
       <Link key={product.id} href={`/shop/${product.slug}`}>
         <div className="relative flex aspect-[1/1]  ">
+          {/* Hvis brugeren hover over det første billede forsvinder det og billede 2 kommer frem */}
           <motion.div className="absolute  h-full w-full" initial={{ opacity: 1 }} style={{ zIndex: 1 }} whileHover={{ opacity: 0 }} transition={{ duration: 1, ease: [0.43, 0.13, 0.23, 0.96] }}>
             <Image src={product.images[0].file.url} alt={product.name} fill />
           </motion.div>
@@ -59,6 +67,7 @@ const ProductShop = ({ product }) => {
         </div>
       </Link>
       <div className="flex flex-col items-center gap-1 text-blackbase ">
+        {/* Her udskriver vi produktets navn, pris og en knap til singleview */}
         <h5 className="font-tyk text-wh5">{product.name}</h5>
         <p className="font-copy text-copy">{product.price} DKK</p>
         <div className="mt-5">
