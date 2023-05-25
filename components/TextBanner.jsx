a00b9f8b5cad087dcddd118da81ec89c477f3e34
Lasse Godtkjaer <lass417r@stud.kea.dk>
2023-05-25 15:38:39 +0200

    om os

--- components/TextBanner.jsx
+++ components/TextBanner.jsx
@@ -3,17 +3,11 @@ import Styles from "../styles/rss.css";
 
 function TextBanner(props) {
   return (
-    <div id="rssBlock" className="flex h-40 items-center">
-      <p className="cnnContents">
-        <span className="marqueeStyle whitespace-nowrap font-tyk text-wh2 text-orangebase">
-          {" "}
-          {props.text}{" "}
-        </span>
+    <div id="rssBlock" className=" flex h-40 items-center whitespace-nowrap">
+      <p className="cnnContents ">
+        <span className="marqueeStyle whitespace-nowrap font-tyk text-wh2 text-orangebase"> {props.text} </span>
 
-        <span className="marqueeStyle2 whitespace-nowrap font-tyk text-wh2 text-orangebase">
-          {" "}
-          {props.text}{" "}
-        </span>
+        <span className="marqueeStyle2 whitespace-nowrap font-tyk text-wh2 text-orangebase"> {props.text} </span>
       </p>
     </div>
   );
--- components/TopBanner.jsx
+++ components/TopBanner.jsx
@@ -1,11 +1,7 @@
-import React from "react";
-
 function TopBanner() {
   return (
     <div className="flex items-center justify-center  bg-greenish py-2">
-      <h6 className="font-tyk text-wh6 text-white100">
-        Køb 2 flasker og få gratis fragt!
-      </h6>
+      <p className="font-tyk text-copy text-white100">Køb 2 flasker og få gratis fragt!</p>
     </div>
   );
 }
