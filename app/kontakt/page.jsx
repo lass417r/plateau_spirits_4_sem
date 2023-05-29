fbad810371bb91f2e3bb7763add8c98d188d5364
Lasse Godtkjaer <lass417r@stud.kea.dk>
2023-05-29 11:25:34 +0200

    maps

--- app/kontakt/page.jsx
+++ app/kontakt/page.jsx
@@ -8,7 +8,6 @@ import mail from "../../public/assets/mail.svg";
 import Kontakt from "../../components/Kontakt";
 import GoogleMap from "../../components/GoogleMaps";
 import TextBanner from "../../components/TextBanner";
-
 import deksel from "../../public/assets/deksel_skrift2.svg";
 
 export default function Shop() {
@@ -17,11 +16,11 @@ export default function Shop() {
       <section className="mx-20 mt-16 flex gap-20">
         <div className="h-[400px] basis-2/3">
           {/* Virker ikke korrekt */}
-          <GoogleMap location="Flæsketorvet 6, 1711 København" />
+          <GoogleMap />
         </div>
 
         <div className="basis-1/2">
-          <h1 className="font-tyk text-mobileH1 md:text-wh1 text-orangebase">Find vej</h1>
+          <h1 className="font-tyk text-mobileH1 text-orangebase md:text-wh1">Find vej</h1>
           <h5 className="text-wh font-tyk text-blackbase"> Adresse: </h5>
           <p className="font-copy text-copy  text-blackbase">
             Flæsketorvet 6<br />
@@ -33,43 +32,25 @@ export default function Shop() {
         <h2 className="text-center font-tyk text-mobileH2 md:text-wh2">Kontakt information</h2>
         <div className="flex-col  justify-between lg:flex lg:flex-row ">
           <div className="flex flex-col justify-center">
-          <div className="flex justify-center p-10">
-          <Image
-            className=""
-            src={mail}
-            width={80}
-            height={80}
-            alt="Billede af chartrøse"
-          />
-          </div>
+            <div className="flex justify-center p-10">
+              <Image className="" src={mail} width={80} height={80} alt="Billede af chartrøse" />
+            </div>
             <h3 className="  flex justify-center font-tyk text-mobileH4 md:text-wh4 lg:mb-8 lg:justify-center">Mail</h3>
             <p className="flex justify-center text-center font-copy text-copy lg:w-80 lg:justify-center">Info@plateauspirits.com</p>
           </div>
 
           <div className="flex flex-col justify-center">
-          <div className="flex justify-center p-10">
-          <Image
-            className=""
-            src={tel}
-            width={80}
-            height={80}
-            alt="Billede af chartrøse"
-          />
-          </div>
+            <div className="flex justify-center p-10">
+              <Image className="" src={tel} width={80} height={80} alt="Billede af chartrøse" />
+            </div>
             <h3 className="flex justify-center text-center font-tyk text-mobileH4 md:text-wh4 lg:mb-8 lg:justify-center">Telefon</h3>
             <p className="flex justify-center text-center font-copy text-copy lg:w-80 lg:justify-center">+45 22 42 20 20</p>
           </div>
           <div className="flex flex-col justify-center">
-          <div className="flex justify-center p-10">
-          <Image
-            className=""
-            src={kodby}
-            width={70}
-            height={80}
-            alt="Billede af chartrøse"
-          />
-          </div>
-            <h3 className="  flex justify-center font-tyk text-mobileH4 md:text-wh4 md:mt-2 lg:mb-8 lg:justify-center">Adresse</h3>
+            <div className="flex justify-center p-10">
+              <Image className="" src={kodby} width={70} height={80} alt="Billede af chartrøse" />
+            </div>
+            <h3 className="  flex justify-center font-tyk text-mobileH4 md:mt-2 md:text-wh4 lg:mb-8 lg:justify-center">Adresse</h3>
             <p className="flex justify-center text-center font-copy text-copy lg:w-80 lg:justify-center">Flæsketorvet 6, 1711 København</p>
           </div>
         </div>
@@ -77,7 +58,7 @@ export default function Shop() {
       <section className="">
         <div className="flex justify-center p-7">
           <div className="flex-col justify-center">
-            <h2 className="font-tyk text-mobileH1 md:text-wh1 text-blackbase">Kontaktformular</h2>
+            <h2 className="font-tyk text-mobileH1 text-blackbase md:text-wh1">Kontaktformular</h2>
             <Kontakt></Kontakt>
             {/* <Image src={deksel} width={442} height={439} alt="Dekorativ illustration"></Image> */}
           </div>
