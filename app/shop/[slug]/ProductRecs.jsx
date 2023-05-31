03c7a9473e4b2033d126d98b8738a8ca7f868f98
Lasse Godtkjaer <lass417r@stud.kea.dk>
2023-05-31 10:05:53 +0200

    products recs

--- app/shop/[slug]/ProductRecs.jsx
+++ app/shop/[slug]/ProductRecs.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import Image from "next/image";
 import Button from "../../../components/Button";
 
@@ -14,7 +15,9 @@ function ProductRecs(props) {
       </div>
       <div className="flex w-auto flex-row gap-5 overflow-scroll p-5 md:gap-14 md:overflow-hidden md:pb-28">
         <div className=" flex w-[100%]  flex-col items-center border border-solid border-orangebase outline-2 md:basis-1/3">
-          <Image className="hover:cursor-mincursor" src={gluna} width={400} height={463} alt="Flot billede af Gin flaske"></Image>
+          <Link href={"/shop/la-luna-absint"}>
+            <Image className="hover:cursor-mincursor" src={gluna} width={400} height={463} alt="Flot billede af Gin flaske"></Image>
+          </Link>
           <div className="my-2 flex  flex-col items-center gap-2 p-5 text-center md:my-5 md:gap-5">
             <h5 className="text-mobileH5 font-normal md:text-wh5">La Luna Absint</h5>
             <p className="mx-10 mb-5 font-copy text-copy">445 DKK</p>
@@ -24,7 +27,9 @@ function ProductRecs(props) {
           </div>
         </div>
         <div className="flex w-[100%]  flex-col items-center border border-solid border-orangebase outline-2 md:basis-1/3">
-          <Image className=" hover:cursor-mincursor" src={gtranq} width={400} height={463} alt="Flot billede af Gin flaske"></Image>
+          <Link href={"/shop/touche"}>
+            <Image className=" hover:cursor-mincursor" src={gtranq} width={400} height={463} alt="Flot billede af Gin flaske"></Image>
+          </Link>
           <div className="my-2 flex  flex-col items-center gap-2 p-5 text-center md:my-5 md:gap-5">
             <h5 className="text-mobileH5 font-normal md:text-wh5">Touché Aquavit</h5>
             <p className="mx-10 mb-5 font-copy text-copy">295 DKK</p>
@@ -34,7 +39,9 @@ function ProductRecs(props) {
           </div>
         </div>
         <div className="flex  w-[100%] flex-col items-center border border-solid border-orangebase outline-2 md:basis-1/3">
-          <Image className=" hover:cursor-mincursor" src={gtouche} width={400} height={463} alt="Flot billede af Gin flaske"></Image>
+          <Link href={"/shop/tranquillite-gin"}>
+            <Image className=" hover:cursor-mincursor" src={gtouche} width={400} height={463} alt="Flot billede af Gin flaske"></Image>
+          </Link>
           <div className="my-2 flex flex-col items-center gap-2 p-5 text-center md:my-5 md:gap-5">
             <h5 className="text-mobileH5 font-normal md:text-wh5">Tranquillité Gin</h5>
             <p className="mx-10 mb-5 font-copy text-copy">395 DKK</p>
